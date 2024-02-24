b7cd68032b53b25d915524d0dfd80fcbe3049abc
muradnuriyev001 <muradnuriyev001@gmail.com>
2024-02-24 15:25:23 +0400

    Highlighting matched filter letters in booklist

--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -39,6 +39,21 @@ const BookList = () => {
     return matchesTitle && matchesAuthor && matchesFavorite;
   });
 
+  const highlightMatch = (text, filter) => {
+    if (!filter) return text;
+    const regex = new RegExp(`(${filter})`, "gi");
+    return text.split(regex).map((substring, i) => {
+      if (substring.toLowerCase() === filter.toLowerCase()) {
+        return (
+          <span key={i} className="highlight">
+            {substring}
+          </span>
+        );
+      }
+      return substring;
+    });
+  };
+  
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
@@ -49,7 +64,8 @@ const BookList = () => {
           {filteredBooks.map((book, index) => (
             <li key={book.id}>
               <div className="book-info">
-                {++index}. {book.title} by <strong>{book.author}</strong>
+                {++index}. {highlightMatch(book.title, titleFiter)} by{" "}
+                <strong>{highlightMatch(book.author, authorFilter)}</strong>
               </div>
               <div className="book-actions">
                 <span onClick={() => handleToggleFavorite(book.id)}>
